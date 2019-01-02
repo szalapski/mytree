d466a2e5fdcf340802ec81df1be5c517dec6034c
Patrick Szalapski <patrick.szalapski@genmills.com>
2019-01-02 10:56:05 -0600

    .

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using MyTree.Shared;
 
 namespace MyTree
-{
+{  
+// it seems that the root namespace must match the folder name, otherwise App won't be found.  See also https://github.com/aspnet/AspNetCore/issues/5524 .
+
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
--- Shared/TodoItem.cs
+++ Shared/TodoItem.cs
@@ -1,5 +1,7 @@
-public class TodoItem
-{
-    public string Title { get; set; }
-    public bool IsDone { get; set; }
+namespace MyTree.Shared {
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
 }
